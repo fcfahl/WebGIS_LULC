af6434a50d3ba440d44a856abb58360808c3ad63
fcfahl <fcfahl@gmx.com>
2016-04-29 17:52:31 +0200

    04-29 gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var useref = require('gulp-useref');
 var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
 var jsonServer = require('gulp-json-srv');
+var connect = require('gulp-connect');
 
 var dir_src = 'src',
     dir_dst = 'public';
@@ -20,10 +21,15 @@ var src = {
   html: dir_src + '/index.html',
   js: dir_src + '/js',
   js_files: dir_src + '/js/**/*.js',
-  css: dir_src + '/css/sass/app.sass',
+  sass: dir_src + '/css/sass/app.sass',
+  css: dir_src + '/css',
+  sass_files: dir_src + '/css/sass/*.sass',
   fonts: '',
   img: dir_src + '/img/**/*',
-  json: dir_src + '/db.json'
+  json: dir_src + '/db.json',
+  jade: dir_src + '/jade/**/*.jade',
+  bootstrap_sass: dir_src + '/bower_components/bootstrap/scss/bootstrap-flex.scss',
+  bootstrap_css: dir_src + '/bower_components/bootstrap/dist/css',
 };
 
 var dst = {
@@ -39,9 +45,16 @@ var dst = {
 // SASS compiler
 gulp.task('sass', function(){
   return gulp
-      .src(src.css)
+      .src(src.sass)
       .pipe(sass())
-      .pipe(gulp.dest(dst.css));
+      .pipe(gulp.dest(src.css));
+});
+
+gulp.task('bootstrap', function(){
+  return gulp
+      .src(src.bootstrap_sass)
+      .pipe(sass())
+      .pipe(gulp.dest(src.bootstrap_css));
 });
 
 // Optimize js and css files
@@ -50,9 +63,9 @@ gulp.task('useref', function(){
       .src(src.html)
       .pipe(useref())
       // Minifies only if it's a JavaScript file
-      .pipe(gulpIf('*.js', uglify()))
+      // .pipe(gulpIf('*.js', uglify()))
       // Minifies only if it's a CSS file
-      .pipe(gulpIf('*.css', cssnano()))
+      // .pipe(gulpIf('*.css', cssnano()))
       .pipe(gulp.dest(dst.dir))
 });
 
@@ -63,7 +76,10 @@ gulp.task('copy', function() {
         .pipe(gulp.dest(dst.img));
     gulp
         .src(src.json)
-        .pipe(gulp.dest(dst.dir))
+        .pipe(gulp.dest(dst.dir));
+    gulp
+        .src(src.js_files)
+        .pipe(gulp.dest(dst.js))
 })
 
 // Sync browser
@@ -77,25 +93,39 @@ gulp.task('browserSync', function() {
 })
 
 
-// Watch
-gulp.task('watch', [ 'browserSync', 'useref', 'sass'], function (){
+gulp.task('jserver', function () {
+  console.log('db:', dst.json );
+	jsonServer.start({
+    data: dst.json,
+		port: 3000
+	});
+});
 
-    gulp.watch(src.css, ['sass']);
+gulp.task('webserver', function() {
+  connect.server({
+    port: 3000,
+    livereload: true,
+    data: dst.json,
+  });
+});
+
+// Watch
+gulp.task('watch', [ 'sass', 'browserSync', 'useref'], function (){
+    gulp.watch(src.sass_files, ['sass']);
+    gulp.watch(src.sass_files, ['useref']);
+
     gulp.watch(src.html, ['useref']);
-    gulp.watch(src.js_files , ['useref']);
+    gulp.watch(src.jade , ['useref']);
+
+    gulp.watch(src.js_files , ['copy']);
 
     gulp.watch(dst.html, browserSync.reload);
-    gulp.watch(src.css, browserSync.reload);
+    gulp.watch(dst.html, browserSync.reload);
 });
 
 
-gulp.task('default', function (callback) {
-  jsonServer.start({
-    data: dst.json,
-    port: 3000
-  });
-  console.log('db:', dst.json );
-  runSequence(['copy', 'sass', 'useref', 'browserSync', 'watch'],
-    callback
+gulp.task('default', function () {
+
+  runSequence(['copy', 'jserver', 'sass', 'bootstrap',  'useref', 'browserSync',  'watch']
   )
 })
